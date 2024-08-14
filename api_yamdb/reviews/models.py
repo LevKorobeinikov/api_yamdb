68a52129f9efef65611baacd2289a0c8eca4afa1
LevKorobeinikov <korobeinikoffl@yandex.ru>
2024-08-14 21:17:46 +0500

    готовы необходимые модели для начала работы 2

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.conf import settings
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.conf import settings
 
 from reviews.utilites import current_year
 
