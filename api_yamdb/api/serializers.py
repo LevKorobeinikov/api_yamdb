3c72af881d3003781df02a54d0cc177923ab2c84
MaxMen_Ubuntu <taksamops3@gmail.com>
2024-08-17 15:30:02 +0300

    review

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-from rest_framework.validators import UniqueTogetherValidator
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 
@@ -10,25 +9,28 @@ from reviews.models import Review, Comments
 class ReviewSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
     score = serializers.IntegerField(
-        validators=[MinValueValidator(
-            settings.MIN_VALUE), MaxValueValidator(settings.MAX_SCOPE_VALUE)]
+        validators=[
+            MinValueValidator(limit_value=settings.MIN_VALUE,
+                              message='Минимальная оценка - 1'),
+            MaxValueValidator(limit_value=settings.MAX_SCOPE_VALUE,
+                              message='Максимальная оценка - 10')]
     )
 
-    class Meta:
-        model = Review
-        fields = ['id', 'text', 'author', 'score', 'pubdate']
-
     def validate(self, data):
-        request = self.context.get('request')
+        request = self.context['request']
         if request.method == 'POST':
             author = request.user
-            title_id = self.context.get('view', {}).kwargs.get('title_id')
+            title_id = self.context['view'].kwargs['title_id']
             if Review.objects.filter(author=author,
                                      title_id=title_id).exists():
                 raise serializers.ValidationError(
                     'Вы уже оставляли рецензию'
                 )
             return data
+
+    class Meta:
+        model = Review
+        fields = ['id', 'text', 'author', 'score', 'pub_date']
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -37,3 +39,4 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comments
         fields = '__all__'
+        read_only_fields = ('review',)
