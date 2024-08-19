9aadaa1ac7e66e57c3338876123cbcb020fe9f4b
LevKorobeinikov <korobeinikoffl@yandex.ru>
2024-08-19 21:20:43 +0500

    правки

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
-from reviews.models import Review, Category, Genre, Title
+from reviews.models import Comment, Review, Category, Genre, Title
 from rest_framework.validators import UniqueValidator
 
 from api_yamdb.settings import (COD_MAX_LENGTH, EMAIL_MAX_LENGTH,
@@ -16,12 +16,12 @@ class UserSerializer(serializers.ModelSerializer):
         regex=r'^[\w.@+-]+\Z',
         required=True,
         max_length=USERNAME_MAX_LENGTH,
-        validators=[UniqueValidator(queryset=User.objects.all())]
+        validators=[UniqueValidator(queryset=ProjectUser.objects.all())]
     )
     email = serializers.EmailField(
         required=True,
         max_length=EMAIL_MAX_LENGTH,
-        validators=[UniqueValidator(queryset=User.objects.all())]
+        validators=[UniqueValidator(queryset=ProjectUser.objects.all())]
     )
 
     class Meta:
@@ -34,12 +34,12 @@ class UserCreateSerializer(serializers.Serializer):
         regex=r'^[\w.@+-]+\Z',
         required=True,
         max_length=USERNAME_MAX_LENGTH,
-        validators=[UniqueValidator(queryset=User.objects.all())]
+        validators=[UniqueValidator(queryset=ProjectUser.objects.all())]
     )
     email = serializers.EmailField(
         required=True,
         max_length=EMAIL_MAX_LENGTH,
-        validators=[UniqueValidator(queryset=User.objects.all())]
+        validators=[UniqueValidator(queryset=ProjectUser.objects.all())]
     )
 
     def validate_username(self, value):
@@ -47,14 +47,14 @@ class UserCreateSerializer(serializers.Serializer):
             raise serializers.ValidationError(
                 'Использовать имя me в качестве username запрещено'
             )
-        if User.objects.filter(username=value).exists():
+        if ProjectUser.objects.filter(username=value).exists():
             raise serializers.ValidationError(
                 'Пользователь с данным username уже существует'
             )
         return value
 
     def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
+        if ProjectUser.objects.filter(email=value).exists():
             raise serializers.ValidationError(
                 'Пользователь с данным email уже существует'
             )
@@ -103,7 +103,7 @@ class CommentSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        model = Comments
+        model = Comment
         fields = '__all__'
         read_only_fields = ('review',)
 
