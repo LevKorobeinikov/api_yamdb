d5821c43bf0769ec584cfcf6ad8e2bf5a3365eca
LevKorobeinikov <korobeinikoffl@yandex.ru>
2024-08-23 13:43:20 +0500

    fix

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
 
-from .views import (CategoryViewSet, GenreViewSet,
-                    TitleViewSet, ReviewViewSet, CommentViewSet, UserViewSet)
+from .views import (
+    CategoryViewSet, CommentViewSet,
+    GenreViewSet, ReviewViewSet,
+    TitleViewSet, UserViewSet
+)
 
 
 router_v1 = DefaultRouter()
-router_v1.register(r'users', UserViewSet, basename='users')
+router_v1.register('users', UserViewSet, basename='users')
 router_v1.register(r'categories', CategoryViewSet, basename='categories')
 router_v1.register(r'genres', GenreViewSet, basename='genres')
 router_v1.register(r'titles', TitleViewSet, basename='titles')
@@ -19,7 +21,11 @@ router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
                    ReviewViewSet, basename='reviews')
 
 
+v1_urlpatterns = [
+    path('', include(router_v1.urls)),
+    path('auth/', include('users.urls')),
+]
+
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
-    path('v1/auth/', include('users.urls')),
+    path('v1/', include(v1_urlpatterns)),
 ]
