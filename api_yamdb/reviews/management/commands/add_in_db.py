da0f1f8f9b6c66e6191b72a92db65c2db9b762d8
LevKorobeinikov <korobeinikoffl@yandex.ru>
2024-08-23 17:10:02 +0500

    add script for db

---
+++ api_yamdb/reviews/management/commands/add_in_db.py
@@ -0,0 +1,41 @@
+from csv import DictReader
+from pathlib import Path
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from reviews.models import Category, Comment, Genre, GenreTitle, Review, Title
+from users.models import ProjectUser
+
+MAPPING_DATA = {
+    ProjectUser: 'users.csv',
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    Title: 'titles.csv',
+    GenreTitle: 'genre_title.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv'
+}
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for model, data in MAPPING_DATA.items():
+            csv_path = Path(settings.BASE_DIR) / 'static/data/' / data
+            with open(csv_path, 'r', encoding='utf-8') as csvfile:
+                reader = DictReader(csvfile)
+                for row in reader:
+                    try:
+                        if model == Title:
+                            row['category'] = Category.objects.get(
+                                id=row['category'])
+                        elif model in (Review, Comment):
+                            row['author'] = ProjectUser.objects.get(
+                                id=row['author'])
+                        model.objects.get_or_create(**row)
+                    except (ValueError, KeyError, model.DoesNotExist) as e:
+                        self.stdout.write(self.style.ERROR(
+                            f'Ошибка при загрузке {data}: {e}'
+                        ))
+                self.stdout.write(self.style.SUCCESS(
+                    f'Файл {data} успешно загружен'))
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,9 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from api_yamdb.constants import (ADMIN, EMAIL_MAX_LENGTH,
-                                FIRST_NAME_MAX_LENGTH, LAST_NAME_MAX_LENGTH,
-                                MODERATOR, ROLE_MAX_LENGTH, USER, USER_ROLE,
-                                USERNAME_MAX_LENGTH)
+                                 FIRST_NAME_MAX_LENGTH, LAST_NAME_MAX_LENGTH,
+                                 MODERATOR, ROLE_MAX_LENGTH, USER, USER_ROLE,
+                                 USERNAME_MAX_LENGTH)
 from .validators import validate_username
 
 
